15784cb6889d0c05df3886f0c941a64e2379bf4f
zhangjie <c17600062264@163.com>
2019-06-29 07:35:20 +0800

    进行第二次修改

--- Zhang/src/com/bawei/Hello.java
+++ Zhang/src/com/bawei/Hello.java
@@ -3,5 +3,8 @@ package com.bawei;
 public class Hello {
     public static void main(String[] args){
         System.out.println("你好");
+        System.out.println("youaregreat");
+        System.out.println("Hello.main");
+        System.out.println("args = " );
     }
 }
